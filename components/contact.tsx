e6d0af1a32353e3d211504f3396cf2acc5d139c5
Quanghng <90704201+Quanghng@users.noreply.github.com>
2023-12-05 00:52:39 +0100

    Added email functionality

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,9 +2,11 @@
 
 import React from 'react'
 import SectionHeading from './section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hooks'
+import { sendEmail } from '@/actions/sendEmail'
+import SubmitBtn from './submit-btn'
+import toast from 'react-hot-toast'
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
@@ -33,8 +35,14 @@ export default function Contact() {
       <form 
         className='mt-10 flex flex-col' 
         action={async (formData) => {
-          console.log(formData.get('senderEmail'));
-          console.log(formData.get('message'));
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success('Email sent successfully!');
         }}
       >
         <input
@@ -49,9 +57,9 @@ export default function Contact() {
           name='message'
           className='h-52 my-3 rounded-lg borderBlack p-4' 
           required 
-          maxLength={500} placeholder='Your message'/>
+          maxLength={5000} placeholder='Your message'/>
 
-        <button className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950' type='submit'>Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/>{" "}</button>
+        <SubmitBtn />
       </form>
     </motion.section>
   )
