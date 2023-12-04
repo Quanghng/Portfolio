b35a18ecd17b8dafe4f0b4b770ba19485272bcef
Quanghng <90704201+Quanghng@users.noreply.github.com>
2023-12-04 13:43:53 +0100

    Changed name to useSectionInView

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { useInView } from 'react-intersection-observer';
 import { SectionName } from "./types";
 
-export function useSectionView(sectionName: SectionName, threshold = 0.75) {
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
     threshold,
   });
