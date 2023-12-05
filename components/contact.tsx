5d55f3b628c771a2e20e6389a251270f9d150e92
Quanghng <90704201+Quanghng@users.noreply.github.com>
2023-12-05 14:51:43 +0100

    Implemented dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -30,10 +30,10 @@ export default function Contact() {
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className='text-gray-700 -mt-6'>Please contact me directly at <a className='underline' href='mailto:quanghng0811@gmail.com'>{" "}quanghng0811@gmail.com</a> or through this form.</p>
+      <p className='text-gray-700 -mt-6 dark:text-white/80'>Please contact me directly at <a className='underline' href='mailto:quanghng0811@gmail.com'>{" "}quanghng0811@gmail.com</a> or through this form.</p>
 
       <form 
-        className='mt-10 flex flex-col' 
+        className='mt-10 flex flex-col dark:text-black' 
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -47,7 +47,7 @@ export default function Contact() {
       >
         <input
           name='senderEmail' 
-          className='h-14 px-4 rounded-lg borderBlack' 
+          className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' 
           type='email' 
           required 
           maxLength={500} 
@@ -55,7 +55,7 @@ export default function Contact() {
 
         <textarea 
           name='message'
-          className='h-52 my-3 rounded-lg borderBlack p-4' 
+          className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' 
           required 
           maxLength={5000} placeholder='Your message'/>
 
